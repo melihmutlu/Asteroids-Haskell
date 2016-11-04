7823d55b277fc9e7d5665f013302dc23a55de3b7
melih mutlu <m.melihmutlu@gmail.com>
2016-11-04 18:42:39 +0100

    new data types added

--- src/Model.hs
+++ src/Model.hs
@@ -14,11 +14,22 @@ data World = World {
         movementAction   :: MovementAction,
         shootAction      :: ShootAction
         -- TODO: add more fields here!
+        -- Coordinates
+        player  :: (PlayerLoc, Angle)
+        enemies :: [(EnemyLoc, Angle)]
+        score :: Int 
+        gameStatus :: GameStatus
     }
     
 data RotateAction   = NoRotation | RotateLeft | RotateRight
 data MovementAction = NoMovement | Thrust
 data ShootAction    = Shoot      | DontShoot
+data Angle = Int
+data Point = (Float,Float)
+data PlayerLoc = Player (Point, Point, Point, Point) 
+data EnemyLoc = Enemy (Point,Point,Point,Point)
+data GameStatus = On | Over
 
 initial :: Int -> World
 initial seed = error "implement Model.initial!"
+
